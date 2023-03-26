23710e8a90b685870b2c23b4a7d8edc37e6db70c
Laimne <107036125+Laimne@users.noreply.github.com>
2023-03-26 15:32:54 +0300

    change

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
+import {NavLink, BrowserRouter, Route, Routes } from  "react-router-dom"
+import MainA from "./Components/augalai/MainA"
+import MainO from "./Components/obelys/MainO"
 function App() {
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <BrowserRouter>
+    <nav>
+      <NavLink to="/" >Auksas</NavLink>
+      <NavLink to="/obelys" >Sidabras</NavLink>
+      </nav>
+    <Routes>
+      <Route path="/" element={<MainA/>}></Route>
+      <Route path="/obelys" element={<MainO/>}></Route>
+    </Routes>
+    </BrowserRouter>
     </div>
   );
 }
